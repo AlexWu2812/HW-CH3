b9774da3d0467931f70789943e3e5388d5af12ef
AlexWu2812 <may281228122812@gmail.com>
2022-11-17 20:06:10 +0800

    first upload

---
+++ src/p14top15/test_p15.java
@@ -0,0 +1,15 @@
+package p14top15;
+
+public class test_p15 
+{
+	public static void main(String[] args) 
+	{
+	Car car1;
+	car1 = new Car();
+	
+	int number = 1234;
+	double gasoline = 20.5;
+	
+	car1.setNumGas(number, gasoline);
+	}
+}
---
+++ src/p5top7/test_p05.java
@@ -0,0 +1,15 @@
+package p5top7;
+
+public class test_p05 
+{
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+	}
+}
+
+class Car{
+	int num;
+	double gas;
+}
